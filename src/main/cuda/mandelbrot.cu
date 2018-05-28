65d8be651c66e3fd18aba486fdd390dd366af4d2
Antonin Teichmann <tt.teichmann@gmail.com>
2018-05-28 18:37:19 +0200

    mandelbrot.cu even bigger refactoring

--- src/main/cuda/mandelbrot.cu
+++ src/main/cuda/mandelbrot.cu
@@ -2,16 +2,11 @@
 #include "math.h"
 #include "point.hpp"
 
-using Pointf = Point2D<float>;
-using Point = Point2D<int>;
 typedef unsigned int uint;
+using Pointf = Point2D<float>;
+using Point = Point2D<uint>;
 
-const int BLACK = 0xff000000;
-const int WHITE = 0xffffffff;
-const int PINK = 0xffb469ff;
-const int YELLOW = 0xff00ffff;
-const int GOLD = 0xff00d7ff;
-const int MAX_SS_LEVEL = 256;
+const uint MAX_SS_LEVEL = 256;
 
 #define CUDA_CALL(x) do { if((x) != cudaSuccess) { \
   printf("Mandelbrot: Error at %s:%d\n",__FILE__,__LINE__); \
@@ -21,7 +16,32 @@ const int MAX_SS_LEVEL = 256;
   printf("Mandelbrot: Error at %s:%d\n",__FILE__,__LINE__); \
   return EXIT_FAILURE;}} while(0)
 
+#define DEBUG_MODE
+#ifdef DEBUG_MODE 
+  #define ASSERT(x) assert(x)
+#else 
+  #define ASSERT(x) do {} while(0)
+#endif
 
+#ifndef CUDART_VERSION
+  #error CUDART_VERSION Undefined!
+#elif (CUDART_VERSION >= 9000) //for cuda 9 and later, use __any_sync(__activemask(), predicate) instead, see Programming guide, B.13 for more details
+  #define __ALL(predicate) __all_sync(__activemask(), predicate)
+  #define __ANY(predicate) __any_sync(__activemask(), predicate)
+#else
+  #define __ALL(predicate) __all(predicate)
+  #define __ANY(predicate) __any(predicate)
+#endif
+
+
+class ColorsARGB{
+public:
+  static constexpr const uint BLACK = 0xff000000;
+  static constexpr const uint WHITE = 0xffffffff;
+  static constexpr const uint PINK  = 0xffb469ff;
+  static constexpr const uint YELLOW= 0xff00ffff;
+  static constexpr const uint GOLD  = 0xff00d7ff;
+}; 
 
 //Mandelbrot content, using standard mathematical terminology for Mandelbrot set definition, i.e.
 //  f_n = f_{n-1}^2 + c
@@ -31,10 +51,10 @@ const int MAX_SS_LEVEL = 256;
 //    x ... for real part (corresponding to geometric x-axis)
 //    y ... for imag part (corresponding to geometric y-axis)
 
-__device__ __forceinline__ int escape(uint dwell, Pointf c){
+__device__ __forceinline__ uint escape(uint dwell, Pointf c){
   Pointf z(0,0);
   float zx_new;
-  int i = 0;
+  uint i = 0;
   while(i < dwell && z.x*z.x+z.y*z.y < 4){
       zx_new = z.x*z.x-z.y*z.y + c.x;
       z.y = 2*z.x*z.y + c.y; 
@@ -45,10 +65,10 @@ __device__ __forceinline__ int escape(uint dwell, Pointf c){
 }
 
 /// Dispersion in this context is "Index of dispersion", aka variance-to-mean ratio. See https://en.wikipedia.org/wiki/Index_of_dispersion for more details
-__device__  __forceinline__ float computeDispersion(int* data, int dataLength, float mean){
-  int n = dataLength;
+__device__  __forceinline__ float computeDispersion(uint* data, uint dataLength, float mean){
+  uint n = dataLength;
   float variance = 0;
-  for(int i = 0; i < dataLength; i++){
+  for(uint i = 0; i < dataLength; i++){
     //using numerically stable Two-Pass algorithm, https://en.wikipedia.org/wiki/Algorithms_for_calculating_variance#Two-pass_algorithm
     variance += (data[i]-mean)*(data[i]-mean);
   }
@@ -56,9 +76,9 @@ __device__  __forceinline__ float computeDispersion(int* data, int dataLength, f
   return variance / mean;
 }
 
-__device__ void printParams_debug(cudaSurfaceObject_t surfaceOutput, long outputDataPitch_debug, int width, int height, float left_bottom_x, float left_bottom_y, float right_top_x, float right_top_y, int dwell, int** outputData_debug, cudaSurfaceObject_t colorPalette, int paletteLength, float* randomSamples, int superSamplingLevel, bool adaptiveSS, bool visualiseSS){
-  const int idx_x = blockDim.x * blockIdx.x + threadIdx.x;
-  const int idx_y = blockDim.y * blockIdx.y + threadIdx.y;
+__device__ void printParams_debug(cudaSurfaceObject_t surfaceOutput, long outputDataPitch_debug, uint width, uint height, float left_bottom_x, float left_bottom_y, float right_top_x, float right_top_y, uint dwell, uint** outputData_debug, cudaSurfaceObject_t colorPalette, uint paletteLength, float* randomSamples, uint superSamplingLevel, bool adaptiveSS, bool visualiseSS){
+  const uint idx_x = blockDim.x * blockIdx.x + threadIdx.x;
+  const uint idx_y = blockDim.y * blockIdx.y + threadIdx.y;
   if(idx_x != 0 || idx_y != 0)
     return;
   printf("\n");
@@ -68,7 +88,7 @@ __device__ void printParams_debug(cudaSurfaceObject_t surfaceOutput, long output
   printf("SS lvl:\t%d\n",superSamplingLevel);
 }
 
-__device__ __forceinline__ bool isWithinRadius(int idx_x, int idx_y, int width, int height, int radius, int focus_x, int focus_y){
+__device__ __forceinline__ bool isWithinRadius(uint idx_x, uint idx_y, uint width, uint height, uint radius, uint focus_x, uint focus_y){
   if(__sad(idx_x, focus_x, 0) > radius / 2) return false;
   if(__sad(idx_y, focus_y, 0) > radius / 2) return false;
   else return true;
@@ -80,7 +100,7 @@ __device__ __forceinline__ bool isWithinRadius(int idx_x, int idx_y, int width, 
 
 __device__  long long seed;
 /// Intended for debugging only
-__device__ __forceinline__ int simpleRandom(int val){
+__device__ __forceinline__ uint simpleRandom(uint val){
     long long a = 1103515245;
     long long c = 12345;
     long long m = 4294967295l; //2**32 - 1
@@ -90,33 +110,35 @@ __device__ __forceinline__ int simpleRandom(int val){
 
   /// Computes indexes to acces 2D array, based on threadIdx and blockIdx.
   /// Morover, threads in a warp will be arranged in a rectangle (rather than in single line as with the naive implementation).
-__device__ const Point2D<int> getImageIndexes(){
-  const int threadID = threadIdx.x + threadIdx.y * blockDim.x;
-  const int warpWidth = 4; //user defined constant, representing desired width of the recatangular warp (2,4,8 are only reasonable values for the following formula)
-  const int blockWidth = blockDim.x * warpWidth;
-  assert (blockDim.x == 32); //following formula works only when blockDim.x is 32 
-  const int inblock_idx_x = (-threadID % (warpWidth * warpWidth) + threadID % blockWidth) / warpWidth + threadID % warpWidth;
-  const int inblock_idx_y = (threadID / blockWidth) * warpWidth + (threadID / warpWidth) % warpWidth;
-  const int idx_x = blockDim.x * blockIdx.x + inblock_idx_x;
-  const int idx_y = blockDim.y * blockIdx.y + inblock_idx_y;
+__device__ const Point2D<uint> getImageIndexes(){
+  const uint threadID = threadIdx.x + threadIdx.y * blockDim.x;
+  const uint warpWidth = 4; //user defined constant, representing desired width of the recatangular warp (2,4,8 are only reasonable values for the following formula)
+  const uint blockWidth = blockDim.x * warpWidth;
+  ASSERT (blockDim.x == 32); //following formula works only when blockDim.x is 32 
+  const uint inblock_idx_x = (-threadID % (warpWidth * warpWidth) + threadID % blockWidth) / warpWidth + threadID % warpWidth;
+  const uint inblock_idx_y = (threadID / blockWidth) * warpWidth + (threadID / warpWidth) % warpWidth;
+  const uint idx_x = blockDim.x * blockIdx.x + inblock_idx_x;
+  const uint idx_y = blockDim.y * blockIdx.y + inblock_idx_y;
   // { //debug
-  //   int warpid = threadID / warpSize;
+  //   uint warpid = threadID / warpSize;
   //   if(idx.x < 8 && idx.y < 8){
   //     printf("bw:%d\n", blockWidth);
   //     printf("%d\t%d\t%d\t%d\t%d\n", threadIdx.x, threadIdx.y, threadID ,dx, dy);
   //   }
   // }
-  return Point2D<int>(idx_x, idx_y);
+  return Point2D<uint>(idx_x, idx_y);
 }
 
 extern "C"
-__global__ void fractalRenderMain(int** output, long outputPitch, int width, int height, float left_bottom_x, float left_bottom_y, float right_top_x, float right_top_y, int dwell, int superSamplingLevel, bool adaptiveSS, bool visualiseSS, float* randomSamples, int renderRadius, int focus_x, int focus_y)
+__global__ void fractalRenderMain(uint** output, long outputPitch, uint width, uint height, float left_bottom_x, float left_bottom_y, float right_top_x, float right_top_y, uint dwell, uint superSamplingLevel, bool adaptiveSS, bool visualiseSS, float* randomSamples, uint renderRadius, uint focus_x, uint focus_y)
 // todo: usporadat poradi paramateru, cudaXXObjects predavat pointrem, ne kopirovanim (tohle rozmyslet, mozna je to takhle dobre)
 //  todo ma to fakt hodne pointeru, mnoho z nich je pritom pro vsechny launche stejny - nezdrzuje tohle? omezene registry a tak
 {
   const Point idx = getImageIndexes();
   if(idx.x >= width || idx.y >= height) return;
-
+  // if(idx.x == 0 && idx.y == 0){
+  //   printf();
+  // }
   if(!isWithinRadius(idx.x, idx.y, width, height, renderRadius, focus_x, focus_y)) return;
 
   //printParams_debug( surfaceOutput,  outputDataPitch_debug,  width,  height,  left_bottom_x,  left_bottom_y, right_top_x,  right_top_y, dwell, outputData_debug,  colorPalette, paletteLength, randomSamples,  superSamplingLevel,  adaptiveSS,  visualiseSS);
@@ -125,15 +147,15 @@ __global__ void fractalRenderMain(int** output, long outputPitch, int width, int
   float pixelWidth = (right_top_x - left_bottom_x) / (float) width;
   float pixelHeight = (right_top_y - left_bottom_y) / (float) height;
 
-  const int adaptiveTreshold = 10;
-  int r[adaptiveTreshold];
-  int adaptivnessUsed = 0;
+  const uint adaptiveTreshold = 10;
+  uint r[adaptiveTreshold];
+  uint adaptivnessUsed = 0;
 
-  int escapeTimeSum = 0;
-  int randomSamplePixelsIdx = (idx.y * width + idx.x)*MAX_SS_LEVEL;
+  uint escapeTimeSum = 0;
+  //uint randomSamplePixelsIdx = (idx.y * width + idx.x)*MAX_SS_LEVEL;
   //superSamplingLevel = 1;
-  //assert superSamplingLevel <= MAX_SS_LEVEL
-  for(int i = 0; i < superSamplingLevel; i++){
+  ASSERT (superSamplingLevel <= MAX_SS_LEVEL);
+  for(uint i = 0; i < superSamplingLevel; i++){
     float random_xd = i / (float) superSamplingLevel; //not really random, just uniform
     float random_yd = random_xd;
     //float random_xd = randomSamples[randomSamplePixelsIdx + i];
@@ -141,7 +163,7 @@ __global__ void fractalRenderMain(int** output, long outputPitch, int width, int
     float cx = left_bottom_x + (idx.x + random_xd)  * pixelWidth;
     float cy = right_top_y - (idx.y + random_yd) * pixelHeight;
 
-    int escapeTime = escape(dwell, Pointf(cx, cy));
+    uint escapeTime = escape(dwell, Pointf(cx, cy));
     escapeTimeSum += escapeTime;
     if(i < adaptiveTreshold){
       r[i] = escapeTime;
@@ -150,22 +172,14 @@ __global__ void fractalRenderMain(int** output, long outputPitch, int width, int
     if(i == adaptiveTreshold && adaptiveSS){ //decide whether to continue with supersampling or not
       float mean = escapeTimeSum / (i+1);
       float dispersion = computeDispersion(r, i, mean);
-      int stop = dispersion <= 0.01;
-#ifndef CUDART_VERSION
-#error CUDART_VERSION Undefined!
-#elif (CUDART_VERSION >= 9000) //for cuda 9 and later, use __any_sync(__activemask(), predicate) instead, see Programming guide, B.13 for more details
-      if(__all_sync(__activemask(), stop)){
-#else
-      if(__all(stop)){
-#endif
-        superSamplingLevel = i+1; //effectively disabling high SS and storing info about actual number of samples taken
-        adaptivnessUsed = WHITE; 
-      }else{ //else we are on an chaotic edge, thus as many samples as possible are needed
-        adaptivnessUsed = BLACK;
-      }      
+      __ALL(dispersion <= 0.01);
+      superSamplingLevel = i+1; //effectively disabling high SS and storing info about actual number of samples taken
+      adaptivnessUsed = ColorsARGB::WHITE; 
+    }else{ //else we are on an chaotic edge, thus as many samples as possible are needed
+        adaptivnessUsed = ColorsARGB::BLACK;
     }
   }
-  int mean = escapeTimeSum / superSamplingLevel;  
+  uint mean = escapeTimeSum / superSamplingLevel;  
 
   /*
   if(adaptivnessUsed && visualiseSS){
@@ -178,7 +192,7 @@ __global__ void fractalRenderMain(int** output, long outputPitch, int width, int
       printf("\n");
   }*/
 
-  int* pOutput = ((int*)((char*)output + idx.y * outputPitch)) + idx.x;
+  uint* pOutput = ((uint*)((char*)output + idx.y * outputPitch)) + idx.x;
   *pOutput = mean;
 }
 
@@ -191,15 +205,15 @@ typedef struct color{
 } color_t;
 
 extern "C"
-__global__ void compose(int** inputMain, long inputMainPitch, int** inputBcg, long inputBcgPitch, cudaSurfaceObject_t surfaceOutput, int width, int height, cudaSurfaceObject_t colorPalette, int paletteLength, int dwell, int mainRenderRadius, int focus_x, int focus_y){
-  const int idx_x = blockDim.x * blockIdx.x + threadIdx.x;
-  const int idx_y = blockDim.y * blockIdx.y + threadIdx.y;
+__global__ void compose(uint** inputMain, long inputMainPitch, uint** inputBcg, long inputBcgPitch, cudaSurfaceObject_t surfaceOutput, uint width, uint height, cudaSurfaceObject_t colorPalette, uint paletteLength, uint dwell, uint mainRenderRadius, uint focus_x, uint focus_y){
+  const uint idx_x = blockDim.x * blockIdx.x + threadIdx.x;
+  const uint idx_y = blockDim.y * blockIdx.y + threadIdx.y;
   if(idx_x >= width || idx_y >= height) return;
 
   /*
-  const int blurSize = 4;
+  const uint blurSize = 4;
   
-  const int convolution[blurSize][blurSize] = {
+  const uint convolution[blurSize][blurSize] = {
       //  {1,2,1},
       //  {2,4,2},
       //  {1,2,1}
@@ -207,50 +221,52 @@ __global__ void compose(int** inputMain, long inputMainPitch, int** inputBcg, lo
       {0,1,0},
       {0,0,0}
   };
-  const int convolutionDivisor = 1;
+  const uint convolutionDivisor = 1;
 
-  int sum = 0;
+  uint sum = 0;
   #pragma unroll
-  for(int i = -blurSize/2; i < blurSize/2; i++){ 
+  for(uint i = -blurSize/2; i < blurSize/2; i++){ 
     #pragma unroll
-    for(int j = -blurSize/2; j < blurSize/2; j++){
-      int x = max(0,min(width,idx_x + i));
-      int y = max(0,min(height,idx_y + j));
-      int* pInput1 = (int*)((char*)input1 + y * input1pitch) + x;
+    for(uint j = -blurSize/2; j < blurSize/2; j++){
+      uint x = max(0,min(width,idx_x + i));
+      uint y = max(0,min(height,idx_y + j));
+      uint* pInput1 = (uint*)((char*)input1 + y * input1pitch) + x;
       sum += (*pInput1) * convolution[i+blurSize/2][j+blurSize/2];
     }
   }
-  int result;
+  uint result;
   result = sum / convolutionDivisor;
   */
   //choose result from one or two
 
-  int* pResult;
+  uint* pResult;
   if(isWithinRadius(idx_x, idx_y, width, height, mainRenderRadius, focus_x, focus_y)){
-    pResult = (int*)((char*)inputMain + idx_y * inputMainPitch) + idx_x;
+    pResult = (uint*)((char*)inputMain + idx_y * inputMainPitch) + idx_x;
   }else{
-    pResult = (int*)((char*)inputBcg + idx_y * inputBcgPitch) + idx_x;
+    pResult = (uint*)((char*)inputBcg + idx_y * inputBcgPitch) + idx_x;
   }
-  int result = *pResult;
+  uint result = *pResult;
 
-  int paletteIdx = paletteLength - (abs(result) % paletteLength) - 1;
-  int resultColor;
-  surf2Dread(&resultColor, colorPalette, paletteIdx * sizeof(int), 0);
+  uint paletteIdx = paletteLength - (result % paletteLength) - 1;
+  ASSERT(paletteIdx >=0);
+  ASSERT(paletteIdx < paletteLength);
+  uint resultColor;
+  surf2Dread(&resultColor, colorPalette, paletteIdx * sizeof(uint), 0);
   if(result == dwell)
-    resultColor = YELLOW;
+    resultColor = ColorsARGB::YELLOW;
 
-  surf2Dwrite(resultColor, surfaceOutput, idx_x * sizeof(int), idx_y);
+  surf2Dwrite(resultColor, surfaceOutput, idx_x * sizeof(uint), idx_y);
 }
 
 extern "C"
 __global__ void blur(){}
 
 extern "C"
-__global__ void fractalRenderUnderSampled(int** output, long outputPitch, int width, int height, float left_bottom_x, float left_bottom_y, float right_top_x, float right_top_y, int dwell, int underSamplingLevel)
+__global__ void fractalRenderUnderSampled(uint** output, long outputPitch, uint width, uint height, float left_bottom_x, float left_bottom_y, float right_top_x, float right_top_y, uint dwell, uint underSamplingLevel)
 {
   //work only at every Nth pixel:
-  const int idx_x = (blockDim.x * blockIdx.x + threadIdx.x) * underSamplingLevel;
-  const int idx_y = (blockDim.y * blockIdx.y + threadIdx.y) * underSamplingLevel;
+  const uint idx_x = (blockDim.x * blockIdx.x + threadIdx.x) * underSamplingLevel;
+  const uint idx_y = (blockDim.y * blockIdx.y + threadIdx.y) * underSamplingLevel;
   if(idx_x >= width-underSamplingLevel || idx_y >= height-underSamplingLevel) return;
   
   //We are in a complex plane from (left_bottom) to (right_top), so we scale the pixels to it
@@ -260,12 +276,12 @@ __global__ void fractalRenderUnderSampled(int** output, long outputPitch, int wi
   float cx = left_bottom_x + (idx_x)  * pixelWidth;
   float cy = right_top_y - (idx_y) * pixelHeight;
 
-  int escapeTime = escape(dwell, Pointf(cx, cy));
+  uint escapeTime = escape(dwell, Pointf(cx, cy));
 
-  for(int x = 0; x < underSamplingLevel; x++){
-    for(int y = 0; y < underSamplingLevel; y++){
-      //surf2Dwrite(resultColor, surfaceOutput, (idx_x + x) * sizeof(unsigned int), (idx_y+y));
-      int* pOutput = ((int*)((char*)output + (idx_y+y) * outputPitch)) + (idx_x+x);
+  for(uint x = 0; x < underSamplingLevel; x++){
+    for(uint y = 0; y < underSamplingLevel; y++){
+      //surf2Dwrite(resultColor, surfaceOutput, (idx_x + x) * sizeof(unsigned uint), (idx_y+y));
+      uint* pOutput = ((uint*)((char*)output + (idx_y+y) * outputPitch)) + (idx_x+x);
       *pOutput = escapeTime;
     }
   }
